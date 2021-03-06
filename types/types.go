678cc30c8460866827d4229860d430b4d98b01b4
Kristijonas Zalys <kzalys@gmail.com>
2021-03-06 15:25:40 +0000

    Ommit empty fields in SensorStatus

--- types/types.go
+++ types/types.go
@@ -1,12 +1,12 @@
 package types
 
 type SensorStatus struct {
-	SensorGroup string `json:"sensorGroup" form:"sensorGroup"`
-	SensorAddress string `json:"sensorAddress" form:"sensorAddress"`
-	PushInterval string `json:"pushInterval" form:"pushInterval"`
-	InfluxHost string `json:"influxHost" form:"influxHost"`
-	InfluxPort string `json:"influxPort" form:"influxPort"`
-	InfluxOrg string `json:"influxOrg" form:"influxOrg"`
-	InfluxBucket string `json:"influxBucket" form:"influxBucket"`
+	SensorGroup string `json:"sensorGroup,omitempty" form:"sensorGroup"`
+	SensorAddress string `json:"sensorAddress,omitempty" form:"sensorAddress"`
+	PushInterval string `json:"pushInterval,omitempty" form:"pushInterval"`
+	InfluxHost string `json:"influxHost,omitempty" form:"influxHost"`
+	InfluxPort string `json:"influxPort,omitempty" form:"influxPort"`
+	InfluxOrg string `json:"influxOrg,omitempty" form:"influxOrg"`
+	InfluxBucket string `json:"influxBucket,omitempty" form:"influxBucket"`
 	InfluxToken string `json:"influxToken,omitempty" form:"influxToken"`
 }
