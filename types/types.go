809a15032cf0ff696a3df3a732fe7cdb837b1acc
Kristijonas Zalys <kzalys@gmail.com>
2021-03-12 12:39:30 +0000

    Change PushInterval type to int64

--- types/types.go
+++ types/types.go
@@ -3,7 +3,7 @@ package types
 type SensorStatus struct {
 	SensorGroup   string `json:"sensorGroup,omitempty" form:"sensorGroup"`
 	SensorAddress string `json:"sensorAddress,omitempty" form:"sensorAddress"`
-	PushInterval  int    `json:"pushInterval,omitempty" form:"pushInterval"`
+	PushInterval  int64  `json:"pushInterval,omitempty" form:"pushInterval"`
 	InfluxHost    string `json:"influxHost,omitempty" form:"influxHost"`
 	InfluxPort    string `json:"influxPort,omitempty" form:"influxPort"`
 	InfluxOrg     string `json:"influxOrg,omitempty" form:"influxOrg"`
